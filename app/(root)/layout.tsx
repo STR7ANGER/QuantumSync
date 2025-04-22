4c0981b453cf6e2df5227026ae71b9489ede9b28
Aditya <adityamaurya.2807@gmail.com>
2025-04-22 15:25:19 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Quantum Sync",
+  description: "A Next.js 15 application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          <Navbar />
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
